02a9ceb21869a3f9bb9abc6dab1cbb3f0bdfbdef
Romain Campech <romain.campech@city.ac.uk>
2019-06-12 10:29:14 +0100

    Checks if ImageTransformation already exists

--- app/use_cases/image_transformations/create.rb
+++ app/use_cases/image_transformations/create.rb
@@ -3,6 +3,7 @@ module ImageTransformations
     include SolidUseCase
 
     steps :find_original_image,
+          :find_existing_image_transformation,
           :create_image_transformation
 
     def find_original_image(params)
@@ -12,7 +13,15 @@ module ImageTransformations
       continue params
     end
 
+    def find_existing_image_transformation(params)
+      params[:image_transformation] = params[:original_image].transformations.find_by(specs: params[:specs].to_h)
+
+      continue params
+    end
+
     def create_image_transformation(params)
+      return continue params[:image_transformation] if params[:image_transformation].present?
+
       image = MiniMagick::Image.open(params[:original_image].file.url)
       params[:specs].each do |spec, value|
         image.send(spec, value)
@@ -20,7 +29,7 @@ module ImageTransformations
 
       params[:image_transformation] = ImageTransformation.create!(
                                         original: params[:original_image],
-                                        specs: params[:specs],
+                                        specs: params[:specs].to_h,
                                         file: File.open(image.path),
                                       )
 
