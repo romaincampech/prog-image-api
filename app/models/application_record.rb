fbfbe5b283950b610db8b63bab887c8ea8236476
Romain Campech <romain.campech@city.ac.uk>
2019-06-12 14:16:26 +0100

    Enhanced ImagesPresenter

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def is_an_image?
+    return true if self.is_a?(Image)
+
+    false
+  end
+
+  def is_an_image_transformation?
+    return true if self.is_a?(ImageTransformation)
+
+    false
+  end
 end
--- app/presenters/images_presenter.rb
+++ app/presenters/images_presenter.rb
@@ -1,14 +1,44 @@
 class ImagesPresenter < BasePresenter
   def as_json(*)
     {
-      image: {
-        id: object.id,
-        filename: object.file.original_filename,
-        url: object.file.url,
-        size_bytes: object.file.size,
-        extension: object.file.extension,
-        mime_type: object.file.mime_type,
-      },
+      image: image,
+      original_id: original_id,
+      specs: specs,
+      transformations: transformations,
     }
+  end
+
+  private
+
+  def image
+    {
+      id: object.id,
+      filename: object.file.original_filename,
+      url: object.file.url,
+      size_bytes: object.file.size,
+      extension: object.file.extension,
+      mime_type: object.file.mime_type,
+    }
+  end
+
+  def original_id
+    p object.is_an_image_transformation?
+    return nil unless object.is_an_image_transformation?
+
+    object.original.id
+  end
+
+  def specs
+    return nil unless object.is_an_image_transformation?
+
+    object.specs
+  end
+
+  def transformations
+    return nil unless object.is_an_image?
+
+    object.transformations.map do |transformation|
+      self.class.new(transformation)
+    end
   end
 end
